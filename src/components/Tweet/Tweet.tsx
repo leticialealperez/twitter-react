0a1077e5addfcae7f18eed37ad28af1fb7168993
Anderson Wilmsen <ander.wilmsen98@gmail.com>
2022-07-26 11:27:45 -0300

    Criado buttons e ajustado a caixa de texto do componete

--- src/components/Tweet/Tweet.tsx
+++ src/components/Tweet/Tweet.tsx
@@ -1,12 +1,16 @@
 import React from "react";
 import styled from "styled-components";
 import img from "../../assets/image/image.png";
-import svg from "../../assets/image/verified.svg";
 import VerifiedIcon from "@mui/icons-material/Verified";
+import { IconButton } from "@mui/material";
+import CropRotateIcon from '@mui/icons-material/CropRotate';
+import ChatBubbleOutlineIcon from '@mui/icons-material/ChatBubbleOutline';
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
+import UploadIcon from '@mui/icons-material/Upload';
 
 const TweetStyled = styled.div`
   width: 600px;
-  height: 300px;
+  height: 250px;
   background-color: black;
   border: 1px solid grey;
   display: flex;
@@ -23,19 +27,43 @@ const TweetStyled = styled.div`
     color: white;
     gap: 10px;
     margin-left: 5px;
+
+    h2{
+      font-size: 16px;
+    }
   }
 
   span > h2 {
     color: #bbb;
     font-weight: 400;
+    font-size: 16px;
+    margin-top: 20px
   }
+
+  .caixa-texto {
+    color: #fff;
+    display: flex;
+    text-align: justify;
+    padding-right: 50px;
+    
+    p{
+      margin-top: -5px;
+    }
+  }
+
+  .buttons {
+    display: flex;
+    justify-content: space-around;
+    padding-right: 50px;
+  }
+  
 `;
 
 const Tweet: React.FC = () => {
   return (
     <TweetStyled>
       <div>
-        <img src={img} />
+        <img src={img} alt='logo-twitter'/>
       </div>
       <div>
         <div className="subtitulos">
@@ -46,7 +74,7 @@ const Tweet: React.FC = () => {
             <h2>@TwitterBrasil . 20 de jul</h2>
           </span>
         </div>
-        <div>
+        <div className="caixa-texto">
           <p>
             As conversas sobre as #Eleiçoes2022 estão a todo vapor! Foram 44
             milhões de Tweets no 1º semestre sobre a corrida eleitoral
@@ -54,6 +82,20 @@ const Tweet: React.FC = () => {
             Twitter está reunindo esforços em diferentes frentes, e temos
             novidades para compartilhar
           </p>
+        </div>
+        <div className="buttons">
+        <IconButton color="primary" aria-label="upload picture" component="label">
+          <ChatBubbleOutlineIcon />
+        </IconButton>
+        <IconButton color="primary" aria-label="upload picture" component="label">
+          <CropRotateIcon />
+        </IconButton>
+        <IconButton color="primary" aria-label="upload picture" component="label">
+          <FavoriteBorderIcon />
+        </IconButton>
+        <IconButton color="primary" aria-label="upload picture" component="label">
+          <UploadIcon />
+        </IconButton>
         </div>
       </div>
     </TweetStyled>
