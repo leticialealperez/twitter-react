0ddfa94c394aa740b22b45b13b057ea717965ee5
Samille Machado <sami.bmachado@gmail.com>
2022-07-26 09:59:04 -0300

    criado component Tweet com estilização da imagem de perfil

--- src/components/Tweet/Tweet.tsx
+++ src/components/Tweet/Tweet.tsx
@@ -1,6 +1,28 @@
-// @flow
-import * as React from "react";
+import React from "react";
+import styled from "styled-components";
+import img from '../../assets/image/image.png'
 
-export const Tweet = () => {
-  return <div></div>;
+const TweetStyled = styled.div`
+  width: 600px;
+  height: 300px;
+  background-color: black;
+  border: 1px solid grey;
+  display: flex;
+  
+  img {
+    width: 75px;
+    border-radius: 100%;
+    margin: 10px;
+  }
+`;
+
+const Tweet: React.FC = () => {
+  return (
+    <TweetStyled>
+      <div><img src={img} /></div>
+      <div>texto</div>
+    </TweetStyled>
+  );
 };
+
+export default Tweet;
