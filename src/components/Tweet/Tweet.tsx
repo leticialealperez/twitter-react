a2ecf690c40cb20a0b7a573bd4ca758a1eca58b0
Leticia Leal <leticialealp@hotmail.com>
2022-07-28 07:29:09 -0300

    add Material UI VerifiedIcon

--- src/components/Tweet/Tweet.tsx
+++ src/components/Tweet/Tweet.tsx
@@ -1,31 +1,35 @@
 import React from 'react';
 import TweetStyled from './TweetStyled';
+import VerifiedIcon from '@mui/icons-material/Verified';
 
 const Tweet: React.FC = () => {
   const imageUrl = 'https://avatars.githubusercontent.com/u/71561411?v=4';
   return (
     <TweetStyled>
       <div className="tweet-container">
-      <div>
-        <div className="avatar">
-          <img alt="" src={imageUrl} />
-        </div>
-        <div className="tweet-body">
-          <div>
-            <p className="tweet-title">
-              TechHelper Joinha da Growdev &nbsp;
-              <span>
-                 •&nbsp; @xaxalautech &nbsp;
-              </span>
-              <span>
-                •&nbsp; 28 Jul
-              </span>
-            </p>
-            <p className="text-tweet">Ola gente, esse é meu primeiro tweet</p>
+        <div>
+          <div className="avatar">
+            <img alt="" src={imageUrl} />
+          </div>
+          <div className="tweet-body">
+            <div>
+              <p className="tweet-title">
+                TechHelper Joinha da Growdev
+                <span>
+                  <VerifiedIcon fontSize='small' sx={{ marginBottom: '-5px', marginLeft: '2px'}}/>
+                </span> 
+                <span>
+                  &nbsp; @xaxalautech &nbsp;
+                </span>
+                <span>
+                  •&nbsp; 28 Jul
+                </span>
+              </p>
+              <p className="text-tweet">Ola gente, esse é meu primeiro tweet</p>
+            </div>
           </div>
         </div>
       </div>
-    </div>
     </TweetStyled>
   );
 };
